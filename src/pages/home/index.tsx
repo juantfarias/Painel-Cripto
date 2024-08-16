ef14f3fee0f168b29bebd3fb6ddfd27885a4b2e7
Juan Tallys Marques de Farias <juan.farias@PSC.LOCAL>
2024-08-16 13:31:39 -0300

    segundo envio

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,85 +1,157 @@
-
-import { useState, FormEvent } from 'react'
+import { useState , FormEvent, useEffect } from 'react'
 import styles from './home.module.css'
-import { BsSearch } from 'react-icons/bs'
+import { BsSearch } from  'react-icons/bs'
 import { Link, useNavigate } from 'react-router-dom'
 
+interface CoinProps{
+  id: string;
+  name: string;
+  symbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr:string;
+  explorer: string;
+  formatedPrice?: string;
+  formatedMarket?: string;
+  formatedVolume?: string;
+}
+
+interface DataProp{
+  data: CoinProps[]
+}
+
 export function Home() {
-  
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("")
+  const [coins, setCoins] = useState<CoinProps[]>([]);
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    getData();
+  }, [])
+
+  async function getData(){
+    fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+    .then(response => response.json())
+    .then((data: DataProp) => {
+      const coinsData = data.data;
+
+      const price = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+      })
+
+      const priceCompact = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        notation: "compact"
+      })
+
+      const formatedResult = coinsData.map((item) => {
+        const formated = {
+          ...item,
+          formatedPrice: price.format(Number(item.priceUsd)),
+          formatedMarket: priceCompact.format(Number(item.marketCapUsd)),
+          formatedVolume: priceCompact.format(Number(item.volumeUsd24Hr))
+        }
+
+        return formated;
+      })
+
+      //console.log(formatedResult);
+      setCoins(formatedResult);
+
+    })
+
+  }
+
   function handleSubmit(e: FormEvent){
     e.preventDefault();
-    
-    if(input ==="") return;
+
+    if(input === "") return;
 
     navigate(`/detail/${input}`)
-    
   }
+
   function handleGetMore(){
-    alert('teste')
+    alert("TESTE")
   }
 
   return (
-      <main className={styles.container}>
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <input 
-              type="text" 
-              placeholder='Digite o nome da moeda...'
-              value={input}
-              onChange={ (e) => setInput(e.target.value)}
-              />
-            <button type='submit'>
-              <BsSearch size={30} color='#FFF'/>
-            </button>
-          </form>
+    <main className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="Digite o nome da moeda... EX bitcoin"
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
+        />
+        <button type="submit">
+          <BsSearch size={30} color="#FFF" />
+        </button>
+      </form>
 
-          <table>
-            <thead>
-              <th scope='col'>Moeda</th>
-              <th scope='col'>Valor Mercado</th>
-              <th scope='col'>Preço</th>
-              <th scope='col'>Volume</th>
-              <th scope='col'>Mudança 24h</th>
-            </thead>
-            <tbody id='tbody'>
 
-              <tr className={styles.tr}>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24h</th>
+          </tr>
+        </thead>
 
-                <td className={styles.tdLabel} data-Label='Moeda'>
-                  <div className={styles.name}>
-                    <Link to={'/detail/bitcoin'}>
-                     <span>BitCoin</span> | BTC
-                    </Link>
-                  </div>
-                </td>
+        <tbody id="tbody">
 
-                <td className={styles.tdLabel} data-Label='Valor Mercado'>
-                  1T
-                </td>
+          {coins.length > 0 && coins.map((item) => (
+            <tr className={styles.tr} key={item.id}>
 
-                <td className={styles.tdLabel} data-Label='Preço'>
-                  200k
-                </td>
+              <td className={styles.tdLabel} data-label="Moeda">
+                <div className={styles.name}>
+                  <img
+                    className={styles.logo}
+                    alt="Logo Cripto"
+                    src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`}
+                  />
+                  <Link to={`/detail/${item.id}`}>
+                    <span>{item.name}</span> | {item.symbol}
+                  </Link>
+                </div>
+              </td>
+  
+              <td className={styles.tdLabel} data-label="Valor mercado">
+                {item.formatedMarket}
+              </td>
+  
+              <td className={styles.tdLabel} data-label="Preço">
+                {item.formatedPrice}
+              </td>
+  
+              <td className={styles.tdLabel} data-label="Volume">
+                {item.formatedVolume}
+              </td>
+  
+              <td className={Number(item.changePercent24Hr) > 0 ? styles.tdProfit : styles.tdLoss} data-label="Mudança 24h">
+                <span>{Number(item.changePercent24Hr).toFixed(3)}</span>
+              </td>
+  
+            </tr>
+          ))}
 
-                <td className={styles.tdLabel} data-Label='Volume'>
-                  2B
-                </td>
+        </tbody>
+      </table>
 
-                <td className={styles.tdLabel} data-Label='Mudança 24h'>
-                  <span>1.20</span>
-                </td>
-              </tr>
-            </tbody>
-
-          </table>
       <button className={styles.buttonMore} onClick={handleGetMore}>
-        Carregar mais 
+        Carregar mais
       </button>
-      </main>
-  )
-    
-}
 
+    </main>
+  )
+}
